7e3e159cd7b0f12e6a3a5bf675334791ee83c6c3
Leandro Pereira <leandro@daileon.acd.puc-campinas.edu.br>
2008-10-30 14:27:42 -0200

    Fuse MD5 and SHA1 benchmark into one

--- hardinfo2/arch/common/cryptohash.h
+++ hardinfo2/arch/common/cryptohash.h
@@ -15,22 +15,50 @@
  *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
+
+#include <md5.h>
 #include <sha1.h>
 
-static void
-benchmark_sha1(void)
+static void inline md5_step(char *data, glong srclen)
+{
+    struct MD5Context ctx;
+    guchar checksum[16];
+    
+    MD5Init(&ctx);
+    MD5Update(&ctx, (guchar *)data, srclen);
+    MD5Final(checksum, &ctx);
+}
+
+static void inline sha1_step(char *data, glong srclen)
 {
     SHA1_CTX ctx;
     guchar checksum[20];
-    int i;
-    GTimer *timer = g_timer_new();
-    gdouble elapsed = 0;
-    gchar src[65536], *tmpsrc;
-    glong srclen = 65536;
     
-    tmpsrc = src;
+    SHA1Init(&ctx);
+    SHA1Update(&ctx, (guchar*)data, srclen);
+    SHA1Final(checksum, &ctx);
+}
 
-    gchar *bdata_path;
+static gpointer cryptohash_for(unsigned int start, unsigned int end, void *data, GTimer *timer)
+{
+    unsigned int i;
+    
+    for (i = start; i <= end; i++) { 
+        if (i % 2 == 0) {
+            md5_step(data, 65536);
+        } else {
+            sha1_step(data, 65536);
+        }
+    }
+    
+    return NULL;
+}
+
+static void
+benchmark_cryptohash(void)
+{
+    gdouble elapsed = 0;
+    gchar *tmpsrc, *bdata_path;
     
     bdata_path = g_build_filename(params.path_data, "benchmark.data", NULL);
     if (!g_file_get_contents(bdata_path, &tmpsrc, NULL, NULL)) {
@@ -39,24 +67,12 @@ benchmark_sha1(void)
     }     
     
     shell_view_set_enabled(FALSE);
-    shell_status_update("Generating SHA1 sum for 312MiB of data...");
-    
-    for (i = 0; i <= 5000; i++) { 
-        g_timer_start(timer);
-
-        SHA1Init(&ctx);
-        SHA1Update(&ctx, (guchar*)tmpsrc, srclen);
-        SHA1Final(checksum, &ctx);
+    shell_status_update("Running CryptoHash benchmark...");
         
-        g_timer_stop(timer);
-        elapsed += g_timer_elapsed(timer, NULL);
-        
-        shell_status_set_percentage(i/50);
-    }
+    elapsed = benchmark_parallel_for(0, 5000, cryptohash_for, tmpsrc);
     
-    g_timer_destroy(timer);
     g_free(bdata_path);
+    g_free(tmpsrc);
     
-    bench_results[BENCHMARK_SHA1] = 312.0 / elapsed;
+    bench_results[BENCHMARK_CRYPTOHASH] = 312.0 / elapsed;
 }
-
--- hardinfo2/benchmark.c
+++ hardinfo2/benchmark.c
@@ -28,8 +28,7 @@
 enum {
     BENCHMARK_ZLIB,
     BENCHMARK_FIB,
-    BENCHMARK_MD5,
-    BENCHMARK_SHA1,
+    BENCHMARK_CRYPTOHASH,
     BENCHMARK_BLOWFISH,
     BENCHMARK_RAYTRACE,
     BENCHMARK_N_ENTRIES
@@ -38,22 +37,19 @@ enum {
 void scan_zlib(gboolean reload);
 void scan_raytr(gboolean reload);
 void scan_bfsh(gboolean reload);
-void scan_md5(gboolean reload);
+void scan_cryptohash(gboolean reload);
 void scan_fib(gboolean reload);
-void scan_sha1(gboolean reload);
 
 gchar *callback_zlib();
 gchar *callback_raytr();
 gchar *callback_bfsh();
-gchar *callback_md5();
 gchar *callback_fib();
-gchar *callback_sha1();
+gchar *callback_cryptohash();
 
 static ModuleEntry entries[] = {
     {"CPU ZLib", "compress.png", callback_zlib, scan_zlib},
     {"CPU Fibonacci", "module.png", callback_fib, scan_fib},
-    {"CPU MD5", "module.png", callback_md5, scan_md5},
-    {"CPU SHA1", "module.png", callback_sha1, scan_sha1},
+    {"CPU CryptoHash", "module.png", callback_cryptohash, scan_cryptohash},
     {"CPU Blowfish", "blowfish.png", callback_bfsh, scan_bfsh},
     {"FPU Raytracing", "raytrace.png", callback_raytr, scan_raytr},
     {NULL}
@@ -215,8 +211,7 @@ static gdouble bench_results[BENCHMARK_N_ENTRIES];
 
 #include <arch/common/fib.h>
 #include <arch/common/zlib.h>
-#include <arch/common/md5.h>
-#include <arch/common/sha1.h>
+#include <arch/common/cryptohash.h>
 #include <arch/common/blowfish.h>
 #include <arch/common/raytrace.h>
 
@@ -238,22 +233,16 @@ gchar *callback_bfsh()
 				     "CPU Blowfish");
 }
 
-gchar *callback_md5()
+gchar *callback_cryptohash()
 {
-    return benchmark_include_results_reverse(bench_results[BENCHMARK_MD5],
-					     "CPU MD5");
+    return benchmark_include_results_reverse(bench_results[BENCHMARK_CRYPTOHASH],
+					     "CPU Cryptohash");
 }
 
 gchar *callback_fib()
 {
     return benchmark_include_results(bench_results[BENCHMARK_FIB],
 				     "CPU Fibonacci");
-}
-
-gchar *callback_sha1()
-{
-    return benchmark_include_results_reverse(bench_results[BENCHMARK_SHA1],
-					     "CPU SHA1");
 }
 
 #define RUN_WITH_HIGH_PRIORITY(fn)			\
@@ -285,10 +274,10 @@ void scan_bfsh(gboolean reload)
     SCAN_END();
 }
 
-void scan_md5(gboolean reload)
+void scan_cryptohash(gboolean reload)
 {
     SCAN_START();
-    RUN_WITH_HIGH_PRIORITY(benchmark_md5);
+    RUN_WITH_HIGH_PRIORITY(benchmark_cryptohash);
     SCAN_END();
 }
 
@@ -299,21 +288,13 @@ void scan_fib(gboolean reload)
     SCAN_END();
 }
 
-void scan_sha1(gboolean reload)
-{
-    SCAN_START();
-    RUN_WITH_HIGH_PRIORITY(benchmark_sha1);
-    SCAN_END();
-}
-
 const gchar *hi_note_func(gint entry)
 {
     switch (entry) {
     case BENCHMARK_ZLIB:
 	return "Results in KiB/second. Higher is better.";
 
-    case BENCHMARK_MD5:
-    case BENCHMARK_SHA1:
+    case BENCHMARK_CRYPTOHASH:
 	return "Results in MiB/second. Higher is better.";
 
     case BENCHMARK_RAYTRACE:
