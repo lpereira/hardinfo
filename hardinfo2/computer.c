ee9d4e3e88adcb168c8dfbea6050d20ccdc1d91f
Leandro A. F. Pereira <leandro@hardinfo.org>
2009-08-03 13:49:41 -0300

    Fix Virtual Machine detection

--- hardinfo2/computer.c
+++ hardinfo2/computer.c
@@ -317,7 +317,7 @@ gchar *computer_get_virtualization()
          FILE *file;
          
          if ((file = fopen(files[i], "r"))) {
-              while (!found && fgets(buffer, 512, file)) {
+              while (fgets(buffer, 512, file)) {
                   for (j = 0; vm_types[j+1].str; j++) {
                       if (strstr(buffer, vm_types[j].str)) {
                          found = TRUE;
