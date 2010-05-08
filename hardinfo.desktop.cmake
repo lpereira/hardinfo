41791eab560f6c76d18470815c060911584d20de
Leandro A. F. Pereira <leandro@hardinfo.org>
2010-05-08 14:32:12 -0300

    Configure/install hardinfo.desktop in ${PREFIX}/share/applications.

---
+++ hardinfo.desktop.cmake
@@ -0,0 +1,9 @@
+[Desktop Entry]
+Name=System Profiler and Benchmark
+Name[pt_BR]=Informações e Testes do Sistema
+Exec=@CMAKE_INSTALL_PREFIX@/bin/hardinfo
+Icon=@CMAKE_INSTALL_PREFIX@/share/hardinfo/pixmaps/logo.png
+Terminal=false
+Type=Application
+StartupNotify=true
+Categories=System;
