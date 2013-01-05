7bdd4bcf4db4261c709c815eab0165b3bd84f833
Julien Lavergne <gilir@ubuntu.com>
2013-01-05 18:09:10 +0100

    Add a very small fr translation

--- hardinfo.desktop.cmake
+++ hardinfo.desktop.cmake
@@ -2,6 +2,7 @@
 Name=System Profiler and Benchmark
 Name[pt_BR]=Informações e Testes do Sistema
 Name[es]=Informacion y Rendimiento del Sistema
+Name[fr]=Informations et Benchmarks du Système
 Exec=@CMAKE_INSTALL_FULL_BINDIR@/hardinfo
 Icon=@CMAKE_INSTALL_FULL_DATAROOTDIR@/hardinfo/pixmaps/logo.png
 Terminal=false
