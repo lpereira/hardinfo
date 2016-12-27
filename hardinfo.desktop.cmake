8fc73fa5ef9bfb83aeb9174755f5de0ba3742024
TotalCaesar659 <x24cm5b8c54q6szxw@yandex.ru>
2016-12-27 03:46:40 +0300

    Added Russian translation to the shortcut comment

--- hardinfo.desktop.cmake
+++ hardinfo.desktop.cmake
@@ -1,8 +1,9 @@
 [Desktop Entry]
 Name=System Profiler and Benchmark
-Name[pt_BR]=Informações e Testes do Sistema
 Name[es]=Informacion y Rendimiento del Sistema
 Name[fr]=Informations et Benchmarks du Système
+Name[pt_BR]=Informações e Testes do Sistema
+Name[ru]=Сведения о системе и тесты
 Exec=@CMAKE_INSTALL_FULL_BINDIR@/hardinfo
 Icon=@CMAKE_INSTALL_FULL_DATAROOTDIR@/hardinfo/pixmaps/logo.png
 Terminal=false
