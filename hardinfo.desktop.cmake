3a63df7de1b07710028fbcd111ad6f6d0fb564d9
AsciiWolf <mail@asciiwolf.com>
2017-01-11 15:48:36 +0100

    Update hardinfo.desktop.cmake
    Add "Comment" section to fix problems with AppStream.

--- hardinfo.desktop.cmake
+++ hardinfo.desktop.cmake
@@ -4,6 +4,7 @@ Name[es]=Informacion y Rendimiento del Sistema
 Name[fr]=Informations et Benchmarks du Système
 Name[pt_BR]=Informações e Testes do Sistema
 Name[ru]=Информация о системе и тестирование
+Comment=Displays system information
 Exec=@CMAKE_INSTALL_FULL_BINDIR@/hardinfo
 Icon=@CMAKE_INSTALL_FULL_DATAROOTDIR@/hardinfo/pixmaps/logo.png
 Terminal=false
