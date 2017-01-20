d6044bf1dda08e79fea360ddc4cee65878436607
TotalCaesar659 <x24cm5b8c54q6szxw@yandex.ru>
2017-01-20 04:39:49 +0300

    Added Russian translation to the shortcut comment

--- hardinfo.desktop.cmake
+++ hardinfo.desktop.cmake
@@ -5,6 +5,7 @@ Name[fr]=Informations et Benchmarks du Système
 Name[pt_BR]=Informações e Testes do Sistema
 Name[ru]=Информация о системе и тестирование
 Comment=Displays system information
+Comment[ru]=Показывает информацию о системе
 Exec=@CMAKE_INSTALL_FULL_BINDIR@/hardinfo
 Icon=@CMAKE_INSTALL_FULL_DATAROOTDIR@/hardinfo/pixmaps/logo.png
 Terminal=false
