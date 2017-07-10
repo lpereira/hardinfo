ff411c02800774a3f540fdbeadbe213a67b16bca
Burt P <pburt0@gmail.com>
2017-07-09 22:52:41 -0500

    sh (superh): fixes, add tests
    Signed-off-by: Burt P <pburt0@gmail.com>

--- modules/devices/sh/processor.c
+++ modules/devices/sh/processor.c
@@ -50,15 +50,23 @@ processor_scan(void)
             tmp[0] = g_strstrip(tmp[0]);
             tmp[1] = g_strstrip(tmp[1]);
 
-            get_str("machine", processor->model_name);
-            get_str("cpu type", processor->vendor_id);
+            get_str("machine", processor->vendor_id);
+            get_str("cpu type", processor->model_name);
+            get_str("cpu family", processor->family);
+            get_float("cpu clock", processor->cpu_mhz);
+            get_float("bus clock", processor->bus_mhz);
+            get_float("module clock", processor->mod_mhz);
             get_float("bogomips", processor->bogomips);
-            processor->cpu_mhz = processor->bogomips;
         }
         g_strfreev(tmp);
     }
 
     fclose(cpuinfo);
+
+    if (!processor->model_name)
+        processor->model_name = _("SuperH Processor");
+    if (!processor->vendor_id)
+        processor->vendor_id = _("(Unknown)");
 
     return g_slist_append(NULL, processor);
 }
@@ -69,15 +77,21 @@ processor_get_info(GSList *processors)
     Processor *processor = (Processor *)processors->data;
 
     return g_strdup_printf("[%s]\n"
-                        "%s=%s\n"
-                        "%s=%s\n"
+                        "%s=%s\n"      /* cpu type */
+                        "%s=%s\n"      /* machine */
+                        "%s=%s\n"      /* family */
                         "%s=%.2f %s\n" /* frequency */
+                        "%s=%.2f %s\n" /* bus frequency */
+                        "%s=%.2f %s\n" /* module frequency */
                         "%s=%.2f\n"    /* bogomips */
                         "%s=%s\n",     /* byte order */
                     _("Processor"),
-                    _("System Type"), processor->model_name,
-                    _("Model"), processor->vendor_id,
+                    _("Name"), processor->model_name,
+                    _("Machine"), processor->vendor_id,
+                    _("Family"), processor->family,
                     _("Frequency"), processor->cpu_mhz, _("MHz"),
+                    _("Bus Frequency"), processor->bus_mhz, _("MHz"),
+                    _("Module Frequency"), processor->mod_mhz, _("MHz"),
                     _("BogoMips"), processor->bogomips,
                     _("Byte Order"), byte_order_str()
                    );
