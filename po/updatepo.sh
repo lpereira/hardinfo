223f339eedaec9a97de59305d58f4ff9dd7df225
Burt P <pburt0@gmail.com>
2017-07-11 22:37:18 -0500

    Fixup translation support
    * Use `bash updatepo.sh` instead of `make pot`. `make pot` will
      cause absolute paths to appear in hardinfo.pot of whomever last
      ran it.
    * C_() and NC_() support added for contexts
    
    Signed-off-by: Burt P <pburt0@gmail.com>

--- po/updatepo.sh
+++ po/updatepo.sh
@@ -1,3 +1,24 @@
+#!bash
+
+echo "Update gettext translation files."
+
+DER=`pwd`
+if [ ! -e "updatepo.sh" ]
+then
+    echo "Error: Run from po/, the location of hardinfo.pot and XX.po files."
+    exit 1
+fi
+
+mv hardinfo.pot hardinfo.pot.old
+echo "" > hardinfo.pot # empty existing file to join (-j) with
+for d in shell/ modules/ hardinfo/;
+do
+    # work form hardinfo root to get reasonable file reference comments
+    cd ..
+    echo -n `pwd`; echo "/$d ..."
+    find "$d" -type f -name "*.c" -print | xargs xgettext -j -d hardinfo -o "$DER/hardinfo.pot" -k_ -kN_ -kC_:1c,2 -kNC_:1c,2 -c/ --from-code=UTF-8
+    cd "$DER"
+done;
 
 for f in *.po
 do
