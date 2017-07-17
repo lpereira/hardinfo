d8cdefaad50a2789f2f49ea37e23cc4a17ede932
Burt P <pburt0@gmail.com>
2017-07-17 13:31:04 -0500

    net.c: cosmetic changes
    * Mixed tab/space indent -> all spaces indent
    * Remove trailing whitespace
    
    Signed-off-by: Burt P <pburt0@gmail.com>

--- modules/network/net.c
+++ modules/network/net.c
@@ -40,7 +40,7 @@
 #include <linux/wireless.h>
 #else
 #include <net/if.h>
-#endif	/* HAS_LINUX_WE */
+#endif  /* HAS_LINUX_WE */
 
 #include "hardinfo.h"
 #include "network.h"
@@ -57,12 +57,12 @@ struct _NetInfo {
     char broadcast[16];
 
 #ifdef HAS_LINUX_WE
-    char wi_essid[IW_ESSID_MAX_SIZE + 1];   
+    char wi_essid[IW_ESSID_MAX_SIZE + 1];
     int  wi_rate;
     int  wi_mode, wi_status;
     gboolean wi_has_txpower;
     struct iw_param wi_txpower;
-    int	 wi_quality_level, wi_signal_level, wi_noise_level;
+    int  wi_quality_level, wi_signal_level, wi_noise_level;
     gboolean is_wireless;
 #endif
 };
@@ -78,16 +78,16 @@ void get_wireless_info(int fd, NetInfo *netinfo)
   int r, trash;
 
   netinfo->is_wireless = FALSE;
-  
+
   if ((wrls = fopen("/proc/net/wireless", "r"))) {
       while (fgets(wbuf, 256, wrls)) {
           if (strchr(wbuf, ':') && strstr(wbuf, netinfo->name)) {
               gchar *buf1 = wbuf;
-              
+
               netinfo->is_wireless = TRUE;
-              
+
               buf1 = strchr(buf1, ':') + 1;
-              
+
               if (strchr(buf1, '.')) {
                   sscanf(buf1, "%d %d. %d. %d %d %d %d %d %d %d",
                          &(netinfo->wi_status),
@@ -104,7 +104,7 @@ void get_wireless_info(int fd, NetInfo *netinfo)
                          &trash, &trash, &trash, &trash, &trash,
                          &trash);
               }
-              
+
               break;
           }
       }
@@ -113,14 +113,14 @@ void get_wireless_info(int fd, NetInfo *netinfo)
 
   if (!netinfo->is_wireless)
     return;
-  
+
   strncpy(wi_req.ifr_name, netinfo->name, 16);
-  
+
   /* obtain essid */
   wi_req.u.essid.pointer = netinfo->wi_essid;
   wi_req.u.essid.length  = IW_ESSID_MAX_SIZE + 1;
   wi_req.u.essid.flags   = 0;
-  
+
   if (ioctl(fd, SIOCGIWESSID, &wi_req) < 0) {
     strcpy(netinfo->wi_essid, "");
   } else {
@@ -133,7 +133,7 @@ void get_wireless_info(int fd, NetInfo *netinfo)
   } else {
     netinfo->wi_rate = wi_req.u.bitrate.value;
   }
-  
+
   /* obtain operation mode */
   if (ioctl(fd, SIOCGIWMODE, &wi_req) < 0) {
     netinfo->wi_mode = 0;
@@ -144,19 +144,19 @@ void get_wireless_info(int fd, NetInfo *netinfo)
       netinfo->wi_mode = 6;
     }
   }
-  
+
 #if WIRELESS_EXT >= 10
   /* obtain txpower */
   if (ioctl(fd, SIOCGIWTXPOW, &wi_req) < 0) {
     netinfo->wi_has_txpower = FALSE;
   } else {
     netinfo->wi_has_txpower = TRUE;
-          
+
     memcpy(&netinfo->wi_txpower, &wi_req.u.txpower, sizeof(struct iw_param));
   }
 #else
   netinfo->wi_has_txpower = FALSE;
-#endif	/* WIRELESS_EXT >= 10 */
+#endif  /* WIRELESS_EXT >= 10 */
 }
 #endif /* HAS_LINUX_WE */
 
@@ -174,47 +174,47 @@ void get_net_info(char *if_name, NetInfo * netinfo)
     /* MTU */
     strcpy(ifr.ifr_name, if_name);
     if (ioctl(fd, SIOCGIFMTU, &ifr) < 0) {
-	netinfo->mtu = 0;
+    netinfo->mtu = 0;
     } else {
-	netinfo->mtu = ifr.ifr_mtu;
+    netinfo->mtu = ifr.ifr_mtu;
     }
 
     /* HW Address */
     strcpy(ifr.ifr_name, if_name);
     if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
-	memset(netinfo->mac, 0, 8);
+    memset(netinfo->mac, 0, 8);
     } else {
-	memcpy(netinfo->mac, ifr.ifr_ifru.ifru_hwaddr.sa_data, 8);
+    memcpy(netinfo->mac, ifr.ifr_ifru.ifru_hwaddr.sa_data, 8);
     }
 
     /* IP Address */
     strcpy(ifr.ifr_name, if_name);
     if (ioctl(fd, SIOCGIFADDR, &ifr) < 0) {
-	netinfo->ip[0] = 0;
+    netinfo->ip[0] = 0;
     } else {
-	snprintf(netinfo->ip, sizeof(netinfo->ip), "%s",
-		inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
-			  sin_addr));
+    snprintf(netinfo->ip, sizeof(netinfo->ip), "%s",
+        inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
+              sin_addr));
     }
 
     /* Mask Address */
     strcpy(ifr.ifr_name, if_name);
     if (ioctl(fd, SIOCGIFNETMASK, &ifr) < 0) {
-	netinfo->mask[0] = 0;
+    netinfo->mask[0] = 0;
     } else {
-	snprintf(netinfo->mask, sizeof(netinfo->mask), "%s",
-		inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
-			  sin_addr));
+    snprintf(netinfo->mask, sizeof(netinfo->mask), "%s",
+        inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
+              sin_addr));
     }
 
     /* Broadcast Address */
     strcpy(ifr.ifr_name, if_name);
     if (ioctl(fd, SIOCGIFBRDADDR, &ifr) < 0) {
-	netinfo->broadcast[0] = 0;
+    netinfo->broadcast[0] = 0;
     } else {
-	snprintf(netinfo->broadcast, sizeof(netinfo->broadcast), "%s",
-		inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
-			  sin_addr));
+    snprintf(netinfo->broadcast, sizeof(netinfo->broadcast), "%s",
+        inet_ntoa(((struct sockaddr_in *) &ifr.ifr_addr)->
+              sin_addr));
     }
 
 #ifdef HAS_LINUX_WE
@@ -268,14 +268,14 @@ static void net_get_iface_type(gchar * name, gchar ** type, gchar ** icon, NetIn
     if (ni->is_wireless) {
         *type = "Wireless";
         *icon = "wireless";
-        
+
         return;
     }
 #endif
 
     for (i = 0; netdev2type[i].type; i++) {
-	if (g_str_has_prefix(name, netdev2type[i].type))
-	    break;
+    if (g_str_has_prefix(name, netdev2type[i].type))
+        break;
     }
 
     *type = netdev2type[i].label;
@@ -327,13 +327,13 @@ static void scan_net_interfaces_24(void)
     gdouble trans_packets;
 
     if (!g_file_test("/proc/net/dev", G_FILE_TEST_EXISTS)) {
-	if (network_interfaces) {
-	    g_free(network_interfaces);
-	    network_interfaces = g_strdup("[Network Interfaces]\n"
-					  "None found=\n");
-	}
+    if (network_interfaces) {
+        g_free(network_interfaces);
+        network_interfaces = g_strdup("[Network Interfaces]\n"
+                      "None found=\n");
+    }
 
-	return;
+    return;
     }
 
     g_free(network_interfaces);
@@ -348,119 +348,119 @@ static void scan_net_interfaces_24(void)
         return;
 
     while (fgets(buffer, 256, proc_net)) {
-	if (strchr(buffer, ':')) {
-	    gint trash;
-	    gchar ifacename[16];
-	    gchar *buf = buffer;
-	    gchar *iface_type, *iface_icon;
-	    gint i;
+    if (strchr(buffer, ':')) {
+        gint trash;
+        gchar ifacename[16];
+        gchar *buf = buffer;
+        gchar *iface_type, *iface_icon;
+        gint i;
 
-	    buf = g_strstrip(buf);
+        buf = g_strstrip(buf);
 
-	    memset(ifacename, 0, 16);
+        memset(ifacename, 0, 16);
 
-	    for (i = 0; buffer[i] != ':' && i < 16; i++) {
-		ifacename[i] = buffer[i];
-	    }
+        for (i = 0; buffer[i] != ':' && i < 16; i++) {
+        ifacename[i] = buffer[i];
+        }
 
-	    buf = strchr(buf, ':') + 1;
+        buf = strchr(buf, ':') + 1;
 
-	    /* iface: bytes packets errs drop fifo frame compressed multicast */
-	    sscanf(buf, "%lf %lf %lf %d %d %d %d %d %lf %lf %lf",
-		   &recv_bytes, &recv_packets,
-		   &recv_errors, &trash, &trash, &trash, &trash,
-		   &trash, &trans_bytes, &trans_packets, &trans_errors);
+        /* iface: bytes packets errs drop fifo frame compressed multicast */
+        sscanf(buf, "%lf %lf %lf %d %d %d %d %d %lf %lf %lf",
+           &recv_bytes, &recv_packets,
+           &recv_errors, &trash, &trash, &trash, &trash,
+           &trash, &trans_bytes, &trans_packets, &trans_errors);
 
-	    gdouble recv_mb = recv_bytes / 1048576.0;
-	    gdouble trans_mb = trans_bytes / 1048576.0;
+        gdouble recv_mb = recv_bytes / 1048576.0;
+        gdouble trans_mb = trans_bytes / 1048576.0;
 
-	    get_net_info(ifacename, &ni);
+        get_net_info(ifacename, &ni);
 
-	    devid = g_strdup_printf("NET%s", ifacename);
+        devid = g_strdup_printf("NET%s", ifacename);
 
-	    network_interfaces =
-		h_strdup_cprintf
-		("$%s$%s=%s|%.2lfMiB|%.2lfMiB\n",
-		 network_interfaces, devid, ifacename, ni.ip[0] ? ni.ip : "",
-		 trans_mb, recv_mb);
-	    net_get_iface_type(ifacename, &iface_type, &iface_icon, &ni);
+        network_interfaces =
+        h_strdup_cprintf
+        ("$%s$%s=%s|%.2lfMiB|%.2lfMiB\n",
+         network_interfaces, devid, ifacename, ni.ip[0] ? ni.ip : "",
+         trans_mb, recv_mb);
+        net_get_iface_type(ifacename, &iface_type, &iface_icon, &ni);
 
-	    network_icons = h_strdup_cprintf("Icon$%s$%s=%s.png\n",
-					     network_icons, devid,
-					     ifacename, iface_icon);
+        network_icons = h_strdup_cprintf("Icon$%s$%s=%s.png\n",
+                         network_icons, devid,
+                         ifacename, iface_icon);
 
-	    detailed = g_strdup_printf("[Network Adapter Properties]\n"
-				       "Interface Type=%s\n"
-				       "Hardware Address (MAC)=%02x:%02x:%02x:%02x:%02x:%02x\n"
-				       "MTU=%d\n"
-				       "[Transfer Details]\n"
-				       "Bytes Received=%.0lf (%.2fMiB)\n"
-				       "Bytes Sent=%.0lf (%.2fMiB)\n",
-				       iface_type,
-				       ni.mac[0], ni.mac[1],
-				       ni.mac[2], ni.mac[3],
-				       ni.mac[4], ni.mac[5],
-				       ni.mtu,
-				       recv_bytes, recv_mb,
-				       trans_bytes, trans_mb);
-            
+        detailed = g_strdup_printf("[Network Adapter Properties]\n"
+                       "Interface Type=%s\n"
+                       "Hardware Address (MAC)=%02x:%02x:%02x:%02x:%02x:%02x\n"
+                       "MTU=%d\n"
+                       "[Transfer Details]\n"
+                       "Bytes Received=%.0lf (%.2fMiB)\n"
+                       "Bytes Sent=%.0lf (%.2fMiB)\n",
+                       iface_type,
+                       ni.mac[0], ni.mac[1],
+                       ni.mac[2], ni.mac[3],
+                       ni.mac[4], ni.mac[5],
+                       ni.mtu,
+                       recv_bytes, recv_mb,
+                       trans_bytes, trans_mb);
+
 #ifdef HAS_LINUX_WE
-            if (ni.is_wireless) {
-              gchar *txpower;
-              
-              if (ni.wi_has_txpower) {
-                gint mw, dbm;
-              
-                if (ni.wi_txpower.flags & IW_TXPOW_MWATT) {
-                  mw = ni.wi_txpower.value;
-                  dbm = (int) ceil(10.0 * log10((double) ni.wi_txpower.value));
-                } else {
-                  dbm = ni.wi_txpower.value;
-                  mw = (int) floor(pow(10.0, ((double) dbm / 10.0)));
-                }
-                
-                txpower = g_strdup_printf("%ddBm (%dmW)", dbm, mw);
-              } else {
-                txpower = g_strdup("Unknown");
-              }
-            
-              detailed = h_strdup_cprintf("\n[Wireless Properties]\n"
-                                          "Network Name (SSID)=%s\n"
-                                          "Bit Rate=%dMb/s\n"
-                                          "Transmission Power=%s\n"
-                                          "Mode=%s\n"
-                                          "Status=%d\n"
-                                          "Link Quality=%d\n"
-                                          "Signal / Noise=%d dBm / %d dBm (%s)\n",
-                                          detailed,
-                                          ni.wi_essid,
-                                          ni.wi_rate / 1000000,
-                                          txpower,
-                                          wi_operation_modes[ni.wi_mode],
-                                          ni.wi_status,
-                                          ni.wi_quality_level,
-                                          ni.wi_signal_level,
-                                          ni.wi_noise_level,
-                                          wifi_bars(ni.wi_signal_level, ni.wi_noise_level));
+        if (ni.is_wireless) {
+            gchar *txpower;
 
-              g_free(txpower);
+            if (ni.wi_has_txpower) {
+                gint mw, dbm;
+
+                if (ni.wi_txpower.flags & IW_TXPOW_MWATT) {
+                    mw = ni.wi_txpower.value;
+                    dbm = (int) ceil(10.0 * log10((double) ni.wi_txpower.value));
+                } else {
+                    dbm = ni.wi_txpower.value;
+                    mw = (int) floor(pow(10.0, ((double) dbm / 10.0)));
+                }
+
+                txpower = g_strdup_printf("%ddBm (%dmW)", dbm, mw);
+            } else {
+                txpower = g_strdup("Unknown");
             }
+
+            detailed = h_strdup_cprintf("\n[Wireless Properties]\n"
+                "Network Name (SSID)=%s\n"
+                "Bit Rate=%dMb/s\n"
+                "Transmission Power=%s\n"
+                "Mode=%s\n"
+                "Status=%d\n"
+                "Link Quality=%d\n"
+                "Signal / Noise=%d dBm / %d dBm (%s)\n",
+                detailed,
+                ni.wi_essid,
+                ni.wi_rate / 1000000,
+                txpower,
+                wi_operation_modes[ni.wi_mode],
+                ni.wi_status,
+                ni.wi_quality_level,
+                ni.wi_signal_level,
+                ni.wi_noise_level,
+                wifi_bars(ni.wi_signal_level, ni.wi_noise_level));
+
+            g_free(txpower);
+        }
 #endif
 
-	    if (ni.ip[0] || ni.mask[0] || ni.broadcast[0]) {
-		detailed =
-		    h_strdup_cprintf("\n[Internet Protocol (IPv4)]\n"
-				     "IP Address=%s\n" "Mask=%s\n"
-				     "Broadcast Address=%s\n", detailed,
-				     ni.ip[0] ? ni.ip : "Not set",
-				     ni.mask[0] ? ni.mask : "Not set",
-				     ni.broadcast[0] ? ni.
-				     broadcast : "Not set");
-	    }
+        if (ni.ip[0] || ni.mask[0] || ni.broadcast[0]) {
+        detailed =
+            h_strdup_cprintf("\n[Internet Protocol (IPv4)]\n"
+                     "IP Address=%s\n" "Mask=%s\n"
+                     "Broadcast Address=%s\n", detailed,
+                     ni.ip[0] ? ni.ip : "Not set",
+                     ni.mask[0] ? ni.mask : "Not set",
+                     ni.broadcast[0] ? ni.
+                     broadcast : "Not set");
+        }
 
-	    moreinfo_add_with_prefix("NET", devid, detailed);
-	    g_free(devid);
-	}
+        moreinfo_add_with_prefix("NET", devid, detailed);
+        g_free(devid);
+    }
     }
     fclose(proc_net);
 }
