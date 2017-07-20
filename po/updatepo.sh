968b8ad51f2ae0b6bb5feceeec286414b0c4e01a
Burt P <pburt0@gmail.com>
2017-07-20 13:47:34 -0500

    po/updatepo.sh: print stats
    Signed-off-by: Burt P <pburt0@gmail.com>

--- po/updatepo.sh
+++ po/updatepo.sh
@@ -9,6 +9,9 @@ then
     exit 1
 fi
 
+MSGTOTALOLD=`msgattrib --untranslated hardinfo.pot | grep -E "^msgstr \"\"" | wc -l`
+echo "hardinfo.pot has $MSGTOTALOLD strings"
+
 mv hardinfo.pot hardinfo.pot.old
 echo "" > hardinfo.pot # empty existing file to join (-j) with
 for d in hardinfo/ shell/ modules/;
@@ -20,8 +23,21 @@ do
     cd "$DER"
 done;
 
+MSGTOTAL=`msgattrib --untranslated hardinfo.pot | grep -E "^msgstr \"\"" | wc -l`
+TDIFF=$(($MSGTOTAL - $MSGTOTALOLD))
+CHANGE="$TDIFF"
+if [ $TDIFF -gt 0 ]; then CHANGE="+$TDIFF"; fi
+if [ $TDIFF -eq 0 ]; then CHANGE="no change"; fi
+echo "hardinfo.pot now has $MSGTOTAL strings ($CHANGE)"
+
 for f in *.po
 do
     cp "$f" "$f.old"
-    msgmerge -N "$f" hardinfo.pot > tmp.po && mv tmp.po "$f"
+    msgmerge -q -N "$f" hardinfo.pot > tmp.po && mv tmp.po "$f"
+    UNMSG=`msgattrib --untranslated "$f" | grep -E "^msgstr \"\"" | wc -l`
+    DONE=" "
+    if [ $UNMSG -eq 0 ]; then
+       DONE="x"
+    fi
+    echo "- [$DONE] $f : ($UNMSG / $MSGTOTAL remain untranslated)"
 done
