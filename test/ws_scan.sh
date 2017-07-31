fbc62f58697e3cc1b8cf7d90a544b44fddb85db4
Burt P <pburt0@gmail.com>
2017-07-31 00:48:17 -0500

    ws_scan.sh ignore deps/
    Signed-off-by: Burt P <pburt0@gmail.com>

--- test/ws_scan.sh
+++ test/ws_scan.sh
@@ -6,6 +6,6 @@
 cd ..
 grep -lHIrP --include=*.{h,c} -- '^((\t+ +)|( +\t+)|\s+$)' | sed 's/^/- [ ] /' > test/hardinfo-bad.txt
 grep -LHIrP --include=*.{h,c} -- '^((\t+ +)|( +\t+)|\s+$)' | sed 's/^/- [x] /' > test/hardinfo-good.txt
-cat test/hardinfo-bad.txt test/hardinfo-good.txt | LC_ALL=C sort -k 1.5 | grep -vP " (test|build)"
+cat test/hardinfo-bad.txt test/hardinfo-good.txt | LC_ALL=C sort -k 1.5 | grep -vP " (test|build|deps)"
 rm test/hardinfo-bad.txt test/hardinfo-good.txt
 cd test
