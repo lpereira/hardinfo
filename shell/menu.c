3aa3e731b32fbea7e0052c2b5fe370fe9ff88ed9
TotalCaesar659 <x24cm5b8c54q6szxw@yandex.ru>
2018-03-16 03:53:50 +0300

    Update menu.c

--- shell/menu.c
+++ shell/menu.c
@@ -115,52 +115,103 @@ void menu_init(Shell * shell)
     GtkAccelGroup *accel_group;
 
     /* Create our objects */
-    menu_box = shell->vbox;
-    action_group = gtk_action_group_new("HardInfo");
+    /*menu_box = shell->vbox;*/
+    //Removed warning for GTK3
+    /*action_group = gtk_action_group_new("HardInfo");
     menu_manager = gtk_ui_manager_new();
 
     shell->action_group = action_group;
-    shell->ui_manager = menu_manager;
+    shell->ui_manager = menu_manager;*/
 
     /* Pack up our objects:
      * menu_box -> window
      * actions -> action_group
      * action_group -> menu_manager */
-    gtk_action_group_set_translation_domain( action_group, "hardinfo" );//gettext
+    //Removed warning for GTK3
+    /*gtk_action_group_set_translation_domain( action_group, "hardinfo" );//gettext
     gtk_action_group_add_actions(action_group, entries,
 				 G_N_ELEMENTS(entries), NULL);
     gtk_action_group_add_toggle_actions(action_group, toggle_entries,
 					G_N_ELEMENTS(toggle_entries),
 					NULL);
-    gtk_ui_manager_insert_action_group(menu_manager, action_group, 0);
+    gtk_ui_manager_insert_action_group(menu_manager, action_group, 0);*/
 
 
     /* Read in the UI from our XML file */
-    error = NULL;
-    gtk_ui_manager_add_ui_from_string(menu_manager, uidefs_str, -1,
-				      &error);
+    /*error = NULL;*/
+    //Removed warning for GTK3
+    /*gtk_ui_manager_add_ui_from_string(menu_manager, uidefs_str, -1,
+				      &error);*/
 
-    if (error) {
+    /*if (error) {
 	g_error("Building menus failed: %s", error->message);
 	g_error_free(error);
 	return;
-    }
+    }*/
 
     /* Enable menu accelerators */
-    accel_group = gtk_ui_manager_get_accel_group(menu_manager);
-    gtk_window_add_accel_group(GTK_WINDOW(shell->window), accel_group);
+    //Removed warning for GTK3
+    /*accel_group = gtk_ui_manager_get_accel_group(menu_manager);*/
+    //Removed warning for GTK3
+    /*gtk_window_add_accel_group(GTK_WINDOW(shell->window), accel_group);*/
 
     /* Connect up important signals */
     /* This signal is necessary in order to place widgets from the UI manager
      * into the menu_box */
-    g_signal_connect(menu_manager, "add_widget",
-		     G_CALLBACK(menu_add_widget), menu_box);
+    //Removed warning for GTK3
+    /*g_signal_connect(menu_manager, "add_widget",
+		     G_CALLBACK(menu_add_widget), menu_box);*/
 
     /* Show the window and run the main loop, we're done! */
-    gtk_widget_show(menu_box);
+    //Removed warning for GTK3
+    /*gtk_widget_show(menu_box);*/
 
-    gtk_toolbar_set_style(GTK_TOOLBAR
+    //Removed warning for GTK3
+    /*gtk_toolbar_set_style(GTK_TOOLBAR
 			  (gtk_ui_manager_get_widget
 			   (shell->ui_manager, "/MainMenuBarAction")),
-			  GTK_TOOLBAR_BOTH_HORIZ);
+			  GTK_TOOLBAR_BOTH_HORIZ);*/
+	
+	GtkWidget *window;
+    GtkWidget *vbox;
+
+    GtkWidget *menubar;
+    GtkWidget *fileMenu;
+    GtkWidget *fileMi;
+    GtkWidget *quitMi;
+
+    /*gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Simple menu");*/
+
+    /*vbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(shell->window), vbox);*/
+
+    menubar = gtk_menu_bar_new();
+    fileMenu = gtk_menu_new();
+
+    fileMi = gtk_menu_item_new_with_label("File");
+    quitMi = gtk_menu_item_new_with_label("Quit");
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
+    /*gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);*/
+
+    g_signal_connect(G_OBJECT(shell->window), "destroy",
+        G_CALLBACK(gtk_main_quit), NULL);
+
+    g_signal_connect(G_OBJECT(quitMi), "activate",
+        G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show(menubar);
+    /*gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;*/
+	
 }
