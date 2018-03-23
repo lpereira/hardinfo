b0b0cde4e97f2e25914270d833ac069203a6fd30
Burt P <pburt0@gmail.com>
2018-03-23 00:24:38 -0500

    rpi_data: show rcode of unknown pi
    Signed-off-by: Burt P <pburt0@gmail.com>

--- modules/devices/devicetree/rpi_data.c
+++ modules/devices/devicetree/rpi_data.c
@@ -155,7 +155,7 @@ static gchar *rpi_board_details(void) {
                 _("PCB Revision"), rpi_boardinfo[i].pcb,
                 _("Introduction"), rpi_boardinfo[i].intro,
                 _("Manufacturer"), rpi_boardinfo[i].mfg,
-                _("RCode"), rpi_boardinfo[i].value,
+                _("RCode"), (rpi_boardinfo[i].value != unk) ? rpi_boardinfo[i].value : revision,
                 _("SOC (spec)"), rpi_boardinfo[i].soc,
                 _("Memory (spec)"), rpi_boardinfo[i].mem,
                 _("Serial Number"), serial,
