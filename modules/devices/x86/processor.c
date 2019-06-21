d9404d5cb899fcf3e42026a667889992c5b3172b
Burt P <pburt0@gmail.com>
2019-06-21 10:36:50 -0500

    DMI socket/module information moved from Processor to Memory DMI
    As discussed in
    https://github.com/lpereira/hardinfo/issues/345
    
    Signed-off-by: Burt P <pburt0@gmail.com>

--- modules/devices/x86/processor.c
+++ modules/devices/x86/processor.c
@@ -679,11 +679,9 @@ gchar *processor_describe(GSList * processors) {
 }
 
 gchar *dmi_socket_info() {
-    static const char empty_mem_str[] = "No Module Installed";
     gchar *ret = strdup("");
-    unsigned long dt = 4, dtm = 17, i;
+    unsigned long dt = 4, i;
     dmi_handle_list *hl = dmidecode_handles(&dt);
-    dmi_handle_list *hlm = dmidecode_handles(&dtm);
     if (!hl) {
         ret = g_strdup_printf("[%s]\n%s=%s\n",
                 _("Socket Information"), _("Result"),
@@ -721,87 +719,6 @@ gchar *dmi_socket_info() {
         }
 
         dmi_handle_list_free(hl);
-
-        if (hlm) {
-            for(i = 0; i < hlm->count; i++) {
-                unsigned long h = hlm->handles[i];
-                gchar *locator = dmidecode_match("Locator", &dtm, &h);
-                gchar *size_str = dmidecode_match("Size", &dtm, &h);
-
-                if (strcmp(size_str, empty_mem_str) == 0) {
-                    ret = h_strdup_cprintf("[%s (%lu) %s]\n"
-                                    "%s=0x%x\n"
-                                    "%s=%s\n",
-                                    ret,
-                                    _("Memory Socket"), i, locator,
-                                    _("DMI Handle"), h,
-                                    _("Size"), _("(Empty)") );
-                } else {
-                    gchar *form_factor = dmidecode_match("Form Factor", &dtm, &h);
-                    gchar *type = dmidecode_match("Type", &dtm, &h);
-                    gchar *type_detail = dmidecode_match("Type Detail", &dtm, &h);
-                    gchar *speed_str = dmidecode_match("Speed", &dtm, &h);
-                    gchar *configured_clock_str = dmidecode_match("Configured Clock Speed", &dtm, &h);
-                    gchar *voltage_min_str = dmidecode_match("Minimum Voltage", &dtm, &h);
-                    gchar *voltage_max_str = dmidecode_match("Maximum Voltage", &dtm, &h);
-                    gchar *voltage_conf_str = dmidecode_match("Configured Voltage", &dtm, &h);
-                    gchar *mfgr = dmidecode_match("Manufacturer", &dtm, &h);
-                    gchar *partno = dmidecode_match("Part Number", &dtm, &h);
-
-                    gchar *vendor_str = NULL;
-                    if (mfgr) {
-                        const gchar *v_url = vendor_get_url(mfgr);
-                        if (v_url)
-                            vendor_str = g_strdup_printf(" (%s, %s)",
-                                vendor_get_name(mfgr), v_url );
-                        else
-                            vendor_str = g_strdup("");
-                    }
-
-#define UNKIFNULL2(f) ((f) ? f : _("(Unknown)"))
-
-                    ret = h_strdup_cprintf("[%s (%lu) %s]\n"
-                                    "%s=0x%x\n"
-                                    "%s=%s\n"
-                                    "%s=%s%s\n"
-                                    "%s=%s\n"
-                                    "%s=%s / %s\n"
-                                    "%s=%s\n"
-                                    "%s=%s\n"
-                                    "%s=%s\n"
-                                    "%s=%s\n"
-                                    "%s=%s\n"
-                                    "%s=%s\n",
-                                    ret,
-                                    _("Memory Socket"), i, locator,
-                                    _("DMI Handle"), h,
-                                    _("Form Factor"), UNKIFNULL2(form_factor),
-                                    _("Manufacturer"), UNKIFNULL2(mfgr), vendor_str,
-                                    _("Part Number"), UNKIFNULL2(partno),
-                                    _("Type"), UNKIFNULL2(type), UNKIFNULL2(type_detail),
-                                    _("Size"), UNKIFNULL2(size_str),
-                                    _("Speed"), UNKIFNULL2(speed_str),
-                                    _("Configured Clock Frequency"), UNKIFNULL2(configured_clock_str),
-                                    _("Minimum Voltage"), UNKIFNULL2(voltage_min_str),
-                                    _("Maximum Voltage"), UNKIFNULL2(voltage_max_str),
-                                    _("Configured Voltage"), UNKIFNULL2(voltage_conf_str)
-                                    );
-                    g_free(type);
-                    g_free(form_factor);
-                    g_free(speed_str);
-                    g_free(configured_clock_str);
-                    g_free(voltage_min_str);
-                    g_free(voltage_max_str);
-                    g_free(voltage_conf_str);
-                    g_free(mfgr);
-                    g_free(partno);
-                    g_free(vendor_str);
-                }
-                g_free(size_str);
-                g_free(locator);
-            }
-            dmi_handle_list_free(hlm);
-        }
     }
 
     return ret;
