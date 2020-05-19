9227b1829757606cc4e782a4ca6af84b65960a53
Leandro Pereira <leandro@hardinfo.org>
2020-05-19 08:57:03 -0700

    Reposition Synchronize button

--- includes/uidefs.h
+++ includes/uidefs.h
@@ -5,58 +5,64 @@
 
 #if RELEASE
 #define DEBUG_TOOLBAR_ITEMS
-#else 		/* !RELEASE */
-#define DEBUG_TOOLBAR_ITEMS	"<separator/>" \
-                                "<toolitem name=\"ReportBug\" action=\"ReportBugAction\" />"
-#endif		/* !RELEASE */
+#else /* !RELEASE */
+#define DEBUG_TOOLBAR_ITEMS                                                    \
+    "<separator/>"                                                             \
+    "<toolitem name=\"ReportBug\" action=\"ReportBugAction\" />"
+#endif /* !RELEASE */
 
 #ifdef HAS_LIBSOUP
-#define SYNC_MANAGER_MENU_ITEMS "		<separator/>" \
-"		<menuitem name=\"SyncManager\" action=\"SyncManagerAction\" always-show-image=\"true\"/>"
-#define SYNC_MANAGER_TOOL_ITEMS "		<toolitem name=\"SyncManager\" action=\"SyncManagerAction\" />"
+#define SYNC_MANAGER_MENU_ITEMS                                                \
+    "<separator/>"                                                             \
+    "<menuitem name=\"SyncManager\" action=\"SyncManagerAction\" "             \
+    "always-show-image=\"true\"/>"
+#define SYNC_MANAGER_TOOL_ITEMS                                                \
+    "<toolitem name=\"SyncManager\" action=\"SyncManagerAction\"/>"
 
-#else		/* !HAS_LIBSOUP */
+#else /* !HAS_LIBSOUP */
 #define SYNC_MANAGER_MENU_ITEMS
 #define SYNC_MANAGER_TOOL_ITEMS
-#endif		/* !HAS_LIBSOUP */
+#endif /* !HAS_LIBSOUP */
 
-char *uidefs_str = "<ui>" \
-"	<menubar>" \
-"	<menu name=\"InformationMenu\" action=\"InformationMenuAction\">" \
-"		<menuitem name=\"Report\" action=\"ReportAction\" />" \
-"		<menuitem name=\"Copy\" action=\"CopyAction\" />" \
-SYNC_MANAGER_MENU_ITEMS
-"		<separator/>" \
-"		<menuitem name=\"Quit\" action=\"QuitAction\" />" \
-"	</menu>" \
-"	<menu name=\"ViewMenu\" action=\"ViewMenuAction\">" \
-"		<menuitem name=\"SidePane\" action=\"SidePaneAction\"/>" \
-"		<menuitem name=\"Toolbar\" action=\"ToolbarAction\"/>" \
-"		<separator/>"\
-"		<separator name=\"LastSep\"/>" \
-"		<menuitem name=\"Refresh\" action=\"RefreshAction\"/>" \
-"	</menu>" \
-"	<menu name=\"HelpMenu\" action=\"HelpMenuAction\">" \
-"		<menuitem name=\"WebPage\" action=\"HomePageAction\"/>" \
-"		<menuitem name=\"ReportBug\" action=\"ReportBugAction\"/>" \
-"		<separator/>" \
-"		<menu name=\"HelpMenuModules\" action=\"HelpMenuModulesAction\">" \
-"			<separator name=\"LastSep\"/>" \
-"		</menu>" \
-"		<menuitem name=\"About\" action=\"AboutAction\"/>" \
-"	</menu>" \
-"	</menubar>" \
-"	<toolbar action=\"MainMenuBar\" action=\"MainMenuBarAction\">" \
-"		<placeholder name=\"ToolItems\">" \
-"			<toolitem name=\"Refresh\" action=\"RefreshAction\"/>" \
-SYNC_MANAGER_TOOL_ITEMS \
-"			<separator/>" \
-"			<toolitem name=\"Report\" action=\"ReportAction\"/>" \
-"			<toolitem name=\"Copy\" action=\"CopyAction\"/>" \
-"			<separator/>" \
-DEBUG_TOOLBAR_ITEMS \
-"		</placeholder>" \
-"	</toolbar>" \
-"</ui>";
+char *uidefs_str =
+    "<ui>"
+    "	<menubar>"
+    "	<menu name=\"InformationMenu\" action=\"InformationMenuAction\">"
+    "		<menuitem name=\"Report\" action=\"ReportAction\" />"
+    "		<menuitem name=\"Copy\" action=\"CopyAction\" />"
+    SYNC_MANAGER_MENU_ITEMS \
+    "		<separator/>"
+    "		<menuitem name=\"Quit\" action=\"QuitAction\" />"
+    "	</menu>"
+    "	<menu name=\"ViewMenu\" action=\"ViewMenuAction\">"
+    "		<menuitem name=\"SidePane\" action=\"SidePaneAction\"/>"
+    "		<menuitem name=\"Toolbar\" action=\"ToolbarAction\"/>"
+    "		<separator/>"
+    "		<separator name=\"LastSep\"/>"
+    "		<menuitem name=\"Refresh\" action=\"RefreshAction\"/>"
+    "	</menu>"
+    "	<menu name=\"HelpMenu\" action=\"HelpMenuAction\">"
+    "		<menuitem name=\"WebPage\" action=\"HomePageAction\"/>"
+    "		<menuitem name=\"ReportBug\" action=\"ReportBugAction\"/>"
+    "		<separator/>"
+    "		<menu name=\"HelpMenuModules\" "
+    "action=\"HelpMenuModulesAction\">"
+    "			<separator name=\"LastSep\"/>"
+    "		</menu>"
+    "		<menuitem name=\"About\" action=\"AboutAction\"/>"
+    "	</menu>"
+    "	</menubar>"
+    "	<toolbar action=\"MainMenuBar\" action=\"MainMenuBarAction\">"
+    "		<placeholder name=\"ToolItems\">"
+    "			<toolitem name=\"Refresh\" action=\"RefreshAction\"/>"
+    "			<separator/>"
+    "			<toolitem name=\"Report\" action=\"ReportAction\"/>"
+    "			<toolitem name=\"Copy\" action=\"CopyAction\"/>"
+    "			<separator/>"
+    DEBUG_TOOLBAR_ITEMS \
+    SYNC_MANAGER_TOOL_ITEMS \
+    "		</placeholder>"
+    "	</toolbar>"
+    "</ui>";
 
-#endif	/* __UIDEFS_H__ */
+#endif /* __UIDEFS_H__ */
