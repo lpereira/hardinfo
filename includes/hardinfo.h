5996cebeeffad869f3c1bc79dacb87e2ec4d390f
L. Pereira <l@tia.mat.br>
2023-08-14 22:34:22 -0700

    Update copyright year to 2023

--- includes/hardinfo.h
+++ includes/hardinfo.h
@@ -31,7 +31,7 @@
 #include "info.h"
 #include "format_early.h"
 
-#define HARDINFO_COPYRIGHT_LATEST_YEAR 2021
+#define HARDINFO_COPYRIGHT_LATEST_YEAR 2023
 
 #ifndef LOCALEDIR
 #define LOCALEDIR "/usr/share/locale"
