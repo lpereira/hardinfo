26ccb8821315569cf75c55d336d306f2043677a9
bigbear <ns@bigbear.dk>
2024-02-10 13:03:09 +0100

    FIX tools cleaning tool

--- tools/rm_hardinfo.sh
+++ tools/rm_hardinfo.sh
@@ -10,15 +10,15 @@ fi
 remove_hardinfo() {
   BASEDER="$1"
   echo "Removing hardinfo* from $BASEDER ..."
-  rm -rf "$BASEDER/share/hardinfo*"
-  rm -rf "$BASEDER/lib/hardinfo*"
-  rm -f "$BASEDER"/share/locale/*/LC_MESSAGES/hardinfo*.mo
-  rm -f "$BASEDER/bin/hardinfo*"
-  rm -f "$BASEDER/share/applications/hardinfo*.desktop"
-  rm -f "$BASEDER/share/doc/hardinfo*"
-  rm -f "$BASEDER/share/man/man1/hardinfo*.1.gz"
-  rm -f "$BASEDER/share/pixmaps/hardinfo*.xpm"
-  rm -f "$BASEDER/share/menu/hardinfo*"
+  rm -rf $BASEDER/share/hardinfo*
+  rm -rf $BASEDER/lib/hardinfo*
+  rm -f $BASEDER/share/locale/*/LC_MESSAGES/hardinfo*.mo
+  rm -f $BASEDER/bin/hardinfo*
+  rm -f $BASEDER/share/applications/hardinfo*.desktop
+  rm -f $BASEDER/share/doc/hardinfo*
+  rm -f $BASEDER/share/man/man1/hardinfo*.1.gz
+  rm -f $BASEDER/share/pixmaps/hardinfo*.xpm
+  rm -f $BASEDER/share/menu/hardinfo*
   #locate -e hardinfo | grep "$BASEDER"
 }
 
