86f2bbdfeca7ea79378861c157ec6262c2201d25
Super User <root@fedora.bigbear.dk>
2024-03-04 20:53:04 +0100

    PACKAGE Fedora add static spec - error in CPack

--- tools/create_fedora_source.sh
+++ tools/create_fedora_source.sh
@@ -2,26 +2,29 @@
 VERSION=$(cat ../CMakeLists.txt |grep set\(HARDINFO2_VERSION|cut -d '"' -f 2)
 cd ..
 rm -rf build
-yum -y remove hardinfo
+sudo yum -y remove hardinfo
 rm -rf ~/rpmbuild
 
 mkdir build
 cd build
 cmake -DDISTRO=src ..
+cp ../tools/hardinfo.spec .
 make package_source
-cp _CPackage_Packages/Linux-Source/RPM/SPECS/hardinfo.spec .
+#cp _CPack_Packages/Linux-Source/RPM/SPECS/hardinfo.spec .
 
 echo "Fedora Source Package Files ready in build:"
 ls -l hardinfo-$VERSION*.src.rpm
 ls -l hardinfo.spec
 
+#exit
 sleep 3
 
 #checking
-fedpkg --release f39 lint
+#fedpkg --release f39 lint
 
 #install src package
-rpm --nomd5 -i ./hardinfo-$VERSION-1.src.rpm
+sudo rpm --nomd5 -i ./hardinfo-$VERSION-1.src.rpm
+#cp -r ../tools/hardinfo.spec ~/rpmbuild/SPECS/
 
 #create package from srpm
 cd ~/rpmbuild/SPECS
@@ -29,4 +32,5 @@ rpmbuild -ba hardinfo.spec
 
 echo "Fedora binary build from Source Package Files ready:"
 ls -l ~/rpmbuild/RPMS/*
-yum -y install ~/rpmbuild/RPMS/hardinfo-$VERSION*
+sudo yum -y install ~/rpmbuild/RPMS/hardinfo-$VERSION*
+yum info hardinfo
