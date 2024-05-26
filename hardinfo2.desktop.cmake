d972d2bc47cfa69f175ba446b89848dc8a4dc8d8
atzlinux <xsw@atzlinux.com>
2024-05-26 11:11:41 +0800

    Update hardinfo2.desktop.cmake, add zh_CN infos (#42)

--- hardinfo2.desktop.cmake
+++ hardinfo2.desktop.cmake
@@ -1,8 +1,10 @@
 [Desktop Entry]
 Name=Hardinfo2
+Name[zh_CN]=硬件信息
 Comment=System Information and Benchmark
 Comment[de]=Systeminformationen und Benchmark
 Comment[ru]=Информация о системе и тесты
+Comment[zh_CN]=系统信息及性能测试
 Exec=hardinfo2
 Icon=hardinfo2
 Terminal=false
@@ -10,3 +12,4 @@ Type=Application
 StartupNotify=true
 Categories=System;Utility;
 Keywords=linux;kernel;system;hardware;cpu;processor;capabilities;frequency;memory;ram;board;resources;sensors;devices;usb;pci;display;network;benchmark;test;
+Keywords[zh_CN]=linux;内核;系统;硬件;cpu;处理器;capabilities;频率;内存;ram;主板;资源;传感器;设备;usb;pci;显示;网络;性能测试;测试;
