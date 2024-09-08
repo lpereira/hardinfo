8e601d55f4a76bde6548d1df92227e035e01da67
hwspeedy <ns@bigbear.dk>
2024-09-08 09:32:14 +0200

    FIX More Risc-V entries

--- modules/devices/riscv/processor.c
+++ modules/devices/riscv/processor.c
@@ -41,13 +41,26 @@ gchar *processor_name(GSList * processors) {
         char *vendor;
         char *soc;
     } dt_compat_searches[] = {
-        { "thead,light-lpi4a", "T-Head", "TH1520" },
-        { "starfive,visionfive-2", "StarFive", "JH7110" },
+        { "allwinner,sun201-d1", "Allwinner", "SUN20I-D1" },
+        { "sifive,fu540", "SiFive", "FU540" },
+        { "sifive,fu740", "SiFive", "FU740" },
+        { "sophgo,sg2042", "Sophgo", "SG2042" },
+        { "sophgo,cv1800b", "Sophgo", "CV1800B" },
+        { "sophgo,cv1812h", "Sophgo", "CV1812H" },
+        { "starfive,jh7100", "StarFive", "JH7100" },
         { "starfive,jh7110", "StarFive", "JH7110" },
+        { "starfive,visionfive-2", "StarFive", "JH7110" },
+        { "thead,c910", "T-Head", "C910" },
+        { "thead,light-lpi4a", "T-Head", "TH1520" },
 	//
         { "allwinner,", "Allwinner", UNKSOC },
+        { "canaan,", "Canaan", UNKSOC },
+        { "microchip,", "MicroChip", UNKSOC },
+        { "renesas,", "Renesas", UNKSOC },
+        { "sifive,", "SiFive", UNKSOC },
+        { "sophgo,", "Sophgo", UNKSOC },
+        { "starfive,", "StarFive", UNKSOC },
         { "thead,", "T-Head", UNKSOC },
-        { "allwinner,", "Allwinner", UNKSOC },
         { NULL, NULL }
     };
     gchar *ret = NULL;
