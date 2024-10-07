918a24872f0d16c322b546c0f2ebfe636df1ca84
hwspeedy <ns@bigbear.dk>
2024-10-07 21:43:49 +0200

    FIX UI menu/menubar update and cleaning

--- includes/uidefs.h
+++ includes/uidefs.h
@@ -5,13 +5,13 @@
 
 char *uidefs_str =
     "<ui>"
-    "	<menubar>"
+    "	<menubar name=\"MainMenu\">"
     "	<menu name=\"InformationMenu\" action=\"InformationMenuAction\">"
     "		<menuitem name=\"Report\" action=\"ReportAction\" always-show-image=\"true\"/>"
   /*    "		<menuitem name=\"Copy\" action=\"CopyAction\" always-show-image=\"true\"/>"*/
-    "<separator/>"                                                             \
-    "<menuitem name=\"SyncManager\" action=\"SyncManagerAction\" always-show-image=\"true\"/>"
-    "<menuitem name=\"SyncOnStartup\" action=\"SyncOnStartupAction\"/>"
+    "           <separator/>"                                                             \
+    "           <menuitem name=\"SyncManager\" action=\"SyncManagerAction\" always-show-image=\"true\"/>"
+    "           <menuitem name=\"SyncOnStartup\" action=\"SyncOnStartupAction\"/>"
     "		<separator/>"
     "		<menuitem name=\"Quit\" action=\"QuitAction\" always-show-image=\"true\"/>"
     "	</menu>"
@@ -19,15 +19,15 @@ char *uidefs_str =
     "		<menuitem name=\"SidePane\" action=\"SidePaneAction\"/>"
     "		<menuitem name=\"Toolbar\" action=\"ToolbarAction\"/>"
 #if GTK_CHECK_VERSION(3, 0, 0)
-    "	   <menu name=\"ThemeMenu\" action=\"ThemeMenuAction\">"
-    "            <menuitem name=\"DisableTheme\" action=\"DisableThemeAction\"/>"
-    "            <menuitem name=\"Theme1\" action=\"Theme1Action\"/>"
-    "            <menuitem name=\"Theme2\" action=\"Theme2Action\"/>"
-    "            <menuitem name=\"Theme3\" action=\"Theme3Action\"/>"
-    "            <menuitem name=\"Theme4\" action=\"Theme4Action\"/>"
-    "            <menuitem name=\"Theme5\" action=\"Theme5Action\"/>"
-    "            <menuitem name=\"Theme6\" action=\"Theme6Action\"/>"
-    "	   </menu>"
+    "	        <menu name=\"ThemeMenu\" action=\"ThemeMenuAction\">"
+    "               <menuitem name=\"DisableTheme\" action=\"DisableThemeAction\"/>"
+    "               <menuitem name=\"Theme1\" action=\"Theme1Action\"/>"
+    "               <menuitem name=\"Theme2\" action=\"Theme2Action\"/>"
+    "               <menuitem name=\"Theme3\" action=\"Theme3Action\"/>"
+    "               <menuitem name=\"Theme4\" action=\"Theme4Action\"/>"
+    "               <menuitem name=\"Theme5\" action=\"Theme5Action\"/>"
+    "               <menuitem name=\"Theme6\" action=\"Theme6Action\"/>"
+    "	        </menu>"
 #endif
     "		<separator/>"
     "		<separator name=\"LastSep\"/>"
@@ -40,15 +40,14 @@ char *uidefs_str =
     "	</menu>"
     "	</menubar>"
     "	<toolbar action=\"MainMenuBar\" action=\"MainMenuBarAction\">"
-    "		<placeholder name=\"ToolItems\">"
-    "			<toolitem name=\"Refresh\" action=\"RefreshAction\"/>"
+    "			<toolitem name=\"Refresh\" action=\"RefreshAction\" always-show-image=\"true\"/>"
     "			<separator/>"
-    "			<toolitem name=\"Report\" action=\"ReportAction\"/>"
+    "			<toolitem name=\"Report\" action=\"ReportAction\" always-show-image=\"true\"/>"
   /*    "			<toolitem name=\"Copy\" action=\"CopyAction\"/>"*/
     "			<separator/>"
-    "<toolitem name=\"SyncManager\" action=\"SyncManagerAction\" always-show-image=\"true\"/>"
-    "		</placeholder>"
+    "                   <toolitem name=\"SyncManager\" action=\"SyncManagerAction\" always-show-image=\"true\"/>"
     "	</toolbar>"
     "</ui>";
+
 
 #endif /* __UIDEFS_H__ */
