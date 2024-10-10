c9b7506ecda23bd2553df31e4be3320fdbc3ca60
hwspeedy <ns@bigbear.dk>
2024-10-10 20:29:01 +0200

    FIX disable unused module func

--- hardinfo2/util.c
+++ hardinfo2/util.c
@@ -661,8 +661,6 @@ static ShellModule *module_load(gchar * filename)
 	module->weight = weight_func ? weight_func() : 0;
 	module->name = name_func();
 
-        g_module_symbol(module->dll, "hi_module_get_about",
-   	 	        (gpointer) & (module->aboutfunc));
         g_module_symbol(module->dll, "hi_module_deinit",
    	 	        (gpointer) & (module->deinit));
         g_module_symbol(module->dll, "hi_module_get_summary",
