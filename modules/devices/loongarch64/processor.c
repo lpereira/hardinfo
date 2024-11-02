4da6e7843e79bb69520164c1ff2e2e776be8a05e
hwspeedy <ns@bigbear.dk>
2024-11-02 21:22:09 +0100

    FIX Loongarch typos - fixes PR #104

--- modules/devices/loongarch64/processor.c
+++ modules/devices/loongarch64/processor.c
@@ -53,7 +53,7 @@ processor_scan(void)
 
     fclose(cpuinfo);
 
-    return g_slist_append(NULL, processor);q
+    return g_slist_append(NULL, processor);
 }
 
 gchar *processor_name(GSList * processors) {
@@ -68,7 +68,7 @@ gchar *processor_name(GSList * processors) {
         g_free(compat);
     }
 
-    if(!ret) ret=g_strdup("Loongarch64 Processor (%s/NoDT)",processor_name_default_(processors));
+    if(!ret) ret=g_strdup_printf("Loongarch64 Processor (%s/NoDT)",processor_name_default(processors));
     return ret;
 }
 
