06a9ad2386df9e31f1f08439e0b727b9fd670d7d
hwspeedy <ns@bigbear.dk>
2025-03-04 22:02:16 +0100

    FIX Simplify arch "cpack" build

--- tools/arch_makepkg.sh
+++ tools/arch_makepkg.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
-#called in build dir
+#"CPACK" - called in build dir
 echo "Building Arch Package"
-cd ../..
-cp -f hardinfo2/tools/PKGBUILD .
-cp -f hardinfo2/tools/hardinfo2.install .
-tar -czf hardinfo2.tgz hardinfo2
+cd ..
+cp -f ./tools/PKGBUILD .
+cp -f ./tools/hardinfo2.install .
 
 #update package version
-VER=$(cat hardinfo2/CMakeLists.txt |grep HARDINFO2_VERSION\ |grep -o -P '(?<=").*(?=")')
+VER=$(cat CMakeLists.txt |grep HARDINFO2_VERSION\ |grep -o -P '(?<=").*(?=")')
 VER="${VER}_ArchLinux"
 sed -i "/pkgver=/c\pkgver=$VER" PKGBUILD
 
 #build and install
 if [ $(id -u) -ne 0 ]; then
-    makepkg -cs
+    makepkg -cs --noextract
 else
-    chmod o=rwx .
-    sudo -u nobody makepkg -cs
-    chmod o= .
+    chown -R nobody ../hardinfo2
+    sudo -u nobody makepkg -cs --noextract
+    chown -R root ../hardinfo2
 fi
 
 #move result to build dir
-cp *.zst hardinfo2/build/
+mv *.zst ./build/
 
 #cleanup
-rm -f *.zst
 rm -f PKGBUILD
 rm -f hardinfo2.install
-rm -f hardinfo2.tgz
 
-cd hardinfo2/build
+cd build
